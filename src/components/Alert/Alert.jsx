302041a723b7694f48df181de0d602621e9e981d
Steve Ariss <steve@pikari.io>
2022-05-16 23:34:53 -0400

    changed success alert to use SweetAlert2

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useRouter } from 'next/router'
+import Swal from 'sweetalert2'
 
 const Alert = () => {
   const {
@@ -11,18 +12,28 @@ const Alert = () => {
   if (!active) return null
 
   if (success) {
-    return (
-      <div
-        className="w-full h-full absolute inset-0 flex justify-center z-10 items-center bg-[rgba(250,250,250,0.95)]"
-        onClick={() => setActive(false)}
-      >
-        <div className="bg-white rounded shadow-xl p-8 block border border-slate-100 relative">
-          <span className="text-green-600 text-2xl text-center mx-auto block">✓</span>
-          <h2 className="text-purple-900 font-bold text-lg">Payment Successful</h2>
-        </div>
-      </div>
-    )
+    Swal.fire({
+      title: 'Yay!',
+      text: 'Payment received 😃',
+      icon: 'success',
+      confirmButtonText: 'Cool',
+      confirmButtonColor: '#450974',
+    })
   }
+
+  // if (success) {
+  //   return (
+  //     <div
+  //       className="w-full h-full absolute inset-0 flex justify-center z-10 items-center bg-[rgba(250,250,250,0.95)]"
+  //       onClick={() => setActive(false)}
+  //     >
+  //       <div className="bg-white rounded shadow-xl p-8 block border border-slate-100 relative">
+  //         <span className="text-green-600 text-2xl text-center mx-auto block">✓</span>
+  //         <h2 className="text-purple-900 font-bold text-lg">Payment Successful</h2>
+  //       </div>
+  //     </div>
+  //   )
+  // }
 
   return null
 }
